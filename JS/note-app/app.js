8dd1bc4740a62652ab6fe6cc2530ac9e2d61aad4
neparkes-81 <nparkes8165@gmail.com>
2022-02-15 11:13:16 -0500

    initial commit

---
+++ JS/note-app/app.js
@@ -0,0 +1,11 @@
+const notesEl = document.querySelector('.notes');
+const editBtn = document.querySelector('.edit');
+const deleteBtn = document.querySelector('.delete');
+const main = notesEl.querySelector('.main');
+const textarea = notesEl.querySelector('textarea');
+
+editBtn.addEventListener('click', () => {
+    main.classList.toggle('hidden');
+    textarea.classList.toggle('hidden');
+})
+
