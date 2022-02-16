089e3fdd0e215d6f94ae3daa70da97ab4978223e
neparkes-81 <nparkes8165@gmail.com>
2022-02-16 09:04:07 -0500

    Added creating and deleting note functionality

--- JS/note-app/app.js
+++ JS/note-app/app.js
@@ -1,15 +1,44 @@
-const notesEl = document.querySelector('.notes');
-const editBtn = document.querySelector('.edit');
-const deleteBtn = document.querySelector('.delete');
-const main = notesEl.querySelector('.main');
-const textarea = notesEl.querySelector('textarea');
+const addBtn = document.getElementById('add');
 
-editBtn.addEventListener('click', () => {
-    main.classList.toggle('hidden');
-    textarea.classList.toggle('hidden');
+addBtn.addEventListener('click', () => {
+    addNewNote();
 })
 
-textarea.addEventListener('input', (e) => {
-    const { value } = e.target;
-    main.innerHTML = marked.parse(value);
-})
+function addNewNote(){
+    const note  = document.createElement('div');
+    note.classList.add('note');
+
+    note.innerHTML = 
+            `<div class="notes">
+                <div class="tools">
+                    <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button class="delete"><i class="fa-solid fa-trash-can"></i></button>
+                </div>
+                <div class="main hidden"></div>
+                <textarea></textarea>
+            </div>`
+            ;
+
+    document.body.appendChild(note);
+        
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+    const main = note.querySelector('.main');
+    const textarea = note.querySelector('textarea');
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');
+        textarea.classList.toggle('hidden');
+    })
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove();
+    })
+
+    textarea.addEventListener('input', (e) => {
+        const { value } = e.target;
+        main.innerHTML = marked.parse(value);
+    })
+
+
+}
